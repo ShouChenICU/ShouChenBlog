29173c98789d840a480732df26c643c887f85d3a
ShouChen <ShouChen@proton.me>
2024-10-31 00:35:38 +0800

    1、更新文章 2、优化界面和交互

--- layouts/main.vue
+++ layouts/main.vue
@@ -5,9 +5,9 @@ const navHeight = useNavHeight()
 <template>
   <div class="flex flex-col-reverse md:flex-row gap-4 px-2 md:px-[10vw] py-4">
     <div class="flex-1">
-      <div class="sticky" :style="{ top: `calc(${navHeight}px + 1rem)` }">
-        <Profile />
-        <PostCategory />
+      <div class="sticky" :style="{ top: `calc(${navHeight}px + 1rem)` }" v-auto-animate>
+        <Profile key="Profile" />
+        <PostCategory key="PostCategory" v-if="!$route.path.startsWith('/post')" />
       </div>
     </div>
     <NuxtPage class="md:w-[75%]" />
